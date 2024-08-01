caae95ba2b5ac4b3ad72b0bf79a98216beab8a2e
seonekr <145427113+seonekr@users.noreply.github.com>
2024-08-01 14:33:01 +0700

    third

--- oversea_backend/settings.py
+++ oversea_backend/settings.py
@@ -7,6 +7,11 @@ load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+
+
 
 
 # Quick-start development settings - unsuitable for production
